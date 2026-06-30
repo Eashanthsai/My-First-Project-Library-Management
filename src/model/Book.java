6cadee630853cac4de414a2299fd1a22ea7a149f
Eashanthsai <eashanthsaineella@gmail.com>
2026-06-30 08:36:36 +0530

    Fixed Some Bugs

--- src/model/Book.java
+++ src/model/Book.java
@@ -1,57 +1,93 @@
 package model;
 
+
 public class Book {
-    
+
+
     private int id;
     private String title;
     private String author;
     private boolean Issued;
 
+
+
     public Book(int id, String title, String author) {
+
         this.id = id;
         this.title = title;
         this.author = author;
         this.Issued = false;
+
     }
+
+
 
     public int getId() {
+
         return id;
+
     }
+
+
 
     public String getTitle() {
+
         return title;
+
     }
+
+
 
     public String getAuthor() {
+
         return author;
-    }
-
-    
-    public void issueBook(){
-
-        Issued=true;
 
     }
 
 
 
-    public void returnBook(){
+    public boolean isIssued() {
 
-        Issued=false;
+        return Issued;
+
+    }
+
+
+
+
+    public boolean issueBook() {
+
+
+        if(!Issued) {
+
+            Issued = true;
+            return true;
+
+        }
+
+
+        return false;
 
     }
 
 
 
-    public void display(){
 
-        System.out.println(
-        id+" | "+
-        title+" | "+
-        author+
-        " | Issued : "+
-        Issued);
+    public void returnBook() {
+
+        Issued = false;
 
     }
+
+
+
+
+    public void display() {
+
+
+        System.out.println( id + " | " + title + " | " + author + " | Issued : " + Issued);
+
+    }
+
 
 }
--- src/service/LibraryService.java
+++ src/service/LibraryService.java
@@ -1,68 +1,113 @@
 package service;
 
-import model.*;
 import exception.*;
 import java.util.ArrayList;
+import model.*;
 
 public class LibraryService {
 
-    ArrayList<Book> books =new ArrayList<>();
 
+    ArrayList<Book> books = new ArrayList<>();
 
     ArrayList<Member> members = new ArrayList<>();
 
+
+
     public void addBook(Book b){
+
         books.add(b);
-    }
-
-public void showBooks(){
-
-    for(Book b:books){
-
-        b.display();
 
     }
 
-}
-
-public void addMember(Member m){
-
-    members.add(m);
-
-}
 
 
-public void issueBook(int id)throws BookNotFoundException,BookAlreadyIssuedException{
+    public void showBooks(){
 
-    for(Book b:books){
+        for(Book b : books){
 
-        if(b.getId()==id){
-            if(b.isIssued()){
-
-            throw new BookAlreadyIssuedException("Book already issued");
-
-}
-
-    b.issueBook();
-        System.out.println( "Book issued"); 
-        return;
-    }
-}
-
-        throw new BookNotFoundException("Book not found");
-
-}
-
-public void returnBook(int id) throws BookNotFoundException{
-    for(Book b:books){
-        if(b.getId()==id){
-            b.returnBook();
-
-        System.out.println("Book returned");
-        return;
+            b.display();
 
         }
-}
-throw new BookNotFoundException("Book not found");
+
     }
+
+
+
+    public void addMember(Member m){
+
+        members.add(m);
+
+    }
+
+
+
+    public void issueBook(int id) 
+    throws BookNotFoundException, BookAlreadyIssuedException {
+
+
+        for(Book b : books){
+
+
+            if(b.getId() == id){
+
+
+                if(!b.issueBook()){
+
+
+                    throw new BookAlreadyIssuedException(
+                    "Book already issued");
+
+
+                }
+
+
+                System.out.println("Book issued");
+
+                return;
+
+
+            }
+
+        }
+
+
+        throw new BookNotFoundException(
+        "Book not found");
+
+
+    }
+
+
+
+
+    public void returnBook(int id) 
+    throws BookNotFoundException {
+
+
+        for(Book b : books){
+
+
+            if(b.getId() == id){
+
+
+                b.returnBook();
+
+
+                System.out.println("Book returned");
+
+                return;
+
+
+            }
+
+        }
+
+
+        throw new BookNotFoundException(
+        "Book not found");
+
+
+    }
+
+
 }
